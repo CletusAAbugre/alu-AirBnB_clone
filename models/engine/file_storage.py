48b05e601a9fb05f0c33968fe48e72d41820e2cf
CletusAAbugre <c.abugre@alustudent.com>
2025-01-17 19:45:26 +0000

    engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,39 +1,34 @@
-#!/usr/bin/python3
-"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
-from models.user import User
-
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
-
-    __file_path = "file.json"
-    __objects = {}
+    __file_path = "file.json"  # Path to the JSON file
+    __objects = {}  # Dictionary to store objects
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Return the dictionary of objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """Add new object to __objects."""
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
+        """Serialize __objects to the JSON file."""
+        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, "w") as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+            json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """Deserialize the JSON file to __objects."""
         try:
             with open(self.__file_path, "r") as f:
                 obj_dict = json.load(f)
-                for obj in obj_dict.values():
-                    cls_name = obj["__class__"]
-                    if cls_name in ["BaseModel", "User"]:
-                        self.new(eval(cls_name)(**obj))
+            for key, value in obj_dict.items():
+                class_name = value["__class__"]
+                obj_class = globals()[class_name]
+                self.__objects[key] = obj_class(**value)
         except FileNotFoundError:
             pass
 
